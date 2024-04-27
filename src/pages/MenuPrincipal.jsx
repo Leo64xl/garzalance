146051b4f4ea2c13371d802df8a3c17fb2e712a4
Marco Miranda <138633209+SykoDev18@users.noreply.github.com>
2024-04-27 14:16:09 -0600

    pequenos cambios

--- src/pages/MenuPrincipal.jsx
+++ src/pages/MenuPrincipal.jsx
@@ -12,8 +12,8 @@ const MenuPrincipal = () => {
     <><><><><nav class="bg-white border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Logo" />
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GarzaLance</span>
         </a>
         <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
@@ -107,7 +107,7 @@ const MenuPrincipal = () => {
         </div>
       </body>
     </><div>
-          
+  
         </div></></><div class="flex items-center justify-center p-4">
         <h1 class="text-lg font-semibold text-zinc-900 dark:text-white">
           Servicios locales recomendados para ti
@@ -141,6 +141,7 @@ const MenuPrincipal = () => {
           <span>🥰</span>
         </h1>
       </div></>
+      
   )
 }
 
