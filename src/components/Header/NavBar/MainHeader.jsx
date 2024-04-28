bc69d41f6ce822963ec1bfe30e53cc9c64a9df59
Marco Miranda <138633209+SykoDev18@users.noreply.github.com>
2024-04-28 02:20:30 -0600

    Avance de la pagina de producto

--- src/components/Header/NavBar/MainHeader.jsx
+++ src/components/Header/NavBar/MainHeader.jsx
@@ -1,19 +1,38 @@
-import logo from "@/assets/images/logo22.png/";
-import AvatarImg from "@/assets/images/image-avatar.png";
-import MenuIcon from "@/components/Icons/MenuIcon";
-import CartIcon from "@/components/Icons/CartIcon";
+import logo from "../NavBar/logo22.png"
+import AvatarImg from "../NavBar/image-avatar.png";
+import CartIcon from "../../Icons/CartIcon";
+import MenuIcon from "../../Icons/MenuIcon";
+import CloseIcon from "../../Icons/CloseIcon";
+import { useState } from "react";
 
 
 const MainHeader = () => {
+
+    const [navClass, setNavClass] = useState("hidden font-bold md:flex md:flex-row md:gap-4 md:mr-auto md:static md:p-0 md:h-auto")
+
+    const handleOpenMenu = () => {
+        setNavClass("absolute top-0 left-0 flex h-full w-4/5 flex-col gap-y-[21px] bg-white p-8 font-bold md:static md:mr-auto md:flex md:h-auto md:flex-row md:gap-4 md:p-0");
+    };
+    const handleCloseMenu = () => {
+        setNavClass("hidden font-bold md:flex md:flex-row md:gap-4 md:mr-auto md:static md:p-0 md:h-auto")
+    };
+
     return (
-        <header className="flex">
-            <button className="md:hidden">
+        <header className="flex container px-4
+        mx-auto items-center gap-4 bg-gray-200 py-8">
+            <button className="md:hidden" onClick={handleOpenMenu} >
                 <MenuIcon />
             </button>
-        <img src={logo} alt="Logo GarzaLance"
-        className="mr-auto md:mr-0" />
-        <nav className="hidden md:block md:mr-auto">
-          <a href="#">Colletions</a>
+        <img 
+        src={logo} 
+        alt="Logo GarzaLance"
+        className="mr-auto mb-1 h-5 md:mr-0" 
+        />
+        <nav className={navClass}>
+            <button className="mb-12 md:hidden" onClick={handleCloseMenu}>
+                <CloseIcon />
+            </button>
+          <a href="#">Collections</a>
           <a href="#">Men</a>
           <a href="#">Woman</a>
           <a href="#">About</a>
@@ -23,7 +42,8 @@ const MainHeader = () => {
           <button>
             <CartIcon />
           </button>
-          <img src={AvatarImg} alt=""></img>
+          <img src={AvatarImg} alt=""
+          className="w-10" />
         </div>
     </header> 
     );
--- src/pages/producto.jsx
+++ src/pages/producto.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import MainProducts from '../components/product/MainProducts';
+import MainHeader from '../components/Header/NavBar/Mainheader';
 
 const producto = () => {
   return (
     <>
-       
+       <MainHeader />
+       <MainProducts />
     </>
   )
 }
